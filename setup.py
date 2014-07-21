f5343d5050639af330d623a9cadcf6712fde1011
XhmikosR <xhmikosr@gmail.com>
2014-07-21 12:16:30 +0300

    setup.py: Add more information and bump version to 1.0.2.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='EvaluationWorkflow', version='1.0',
+    name='EvaluationWorkflow',
+    description='A Trac workflow plugin for MPC-HC',
+    version='1.0.2',
+    url='https://github.com/mpc-hc/evaluation-workflow',
+    download_url='https://github.com/mpc-hc/evaluation-workflow',
+    license='MIT',
+    author='Armada651 <Jules Blok>',
+    author_email='jules.blok@gmail.com',
+    zip_safe=True,
+    platforms='any',
     packages=['evaluation'],
     entry_points={
         'trac.plugins': [
             'workflow = evaluation.workflow',
-            'hider = evaluation.hider',
-        ],
-    },
+            'hider = evaluation.hider'
+        ]
+    }
 )
